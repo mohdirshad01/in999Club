33363f390484db2b391e55bd5f995a8a36a715a0
mohdirshad01 <102645245+mohdirshad01@users.noreply.github.com>
2024-07-24 02:22:58 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
-export const dbUrl = "mongodb+srv://sikkim:sikkim123@botcluester.azzl5zt.mongodb.net/?retryWrites=true&w=majority&appName=botcluester";
-export const botToken = "7143104506:AAFmDENTKHCzCo2jf2LdcT-zpiP-BiMbOTo";
-export const admins = [6723433154, 5547959277]
+export const dbUrl = "mongodb+srv://botsbazaar:SqeIfowwk7xwcmoE@botsbazaar.sjj4mn8.mongodb.net/?retryWrites=true&w=majority&appName=botsbazaar";
+export const botToken = "6580112130:AAEPpOjNr4jFVOc-0vPjVYU8QlGoKI5jdak";
+export const admins = [6966849450, 5547959277]
 export const channels = ["zaker_channel_001", "zaker_channel_002"]
 export const cancelKeyboard = {
     reply_markup: {
