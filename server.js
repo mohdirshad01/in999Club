86135848153ddeb1e7c902295db7f467ca703e1d
mohdirshad01 <102645245+mohdirshad01@users.noreply.github.com>
2024-07-24 02:55:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import connectToDb from "./db/db.js";
 import User from "./models/User.js";
 connectToDb();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 1000;
 app.use(cors());
 export const bot = new TelegramBot(botToken, { polling: true });
 Commands()
